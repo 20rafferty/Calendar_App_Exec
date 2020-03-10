c32275e1ba36463e07c59c1a07c7add9c6ecd9ed
dcorvi <dmcorvi@gmail.com>
2020-03-10 19:20:15 -0400

    added city and state inuts

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-
+from app.forms import CityForm
 # jsonify works the same way that JSON.stringify does
 # request is what allows us to take in parameters in the api call
 from flask import render_template, jsonify, request
@@ -10,12 +10,22 @@ import requests
 
 # index is going to call weather api and show information on front
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     API_KEY = app.config['WEATHER_API_KEY']
     # print(API_KEY)
-
     city = 'boston'
+
+    form = CityForm()
+
+    if form.validate_on_submit():
+
+        city = form.city.data
+
+        state = form.city.data
+
+        city = city + ","+ state + ",US"
+        print(city)
 
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}&units=imperial'
 
@@ -23,7 +33,7 @@ def index():
     response = requests.get(url).json()
 
     print(response)
-    return render_template('index.html', response=response)
+    return render_template('index.html', form=form, response=response)
 
 @app.route('/calendar')
 def calendar():
